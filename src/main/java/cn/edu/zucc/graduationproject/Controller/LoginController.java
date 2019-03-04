118dbf7e0425116cf8e47d76aee6fda4143544fb
Hesperuse <cjc17774009415@163.com>
2019-03-04 18:31:45 +0800

    test code

---
+++ src/main/java/cn/edu/zucc/graduationproject/Controller/LoginController.java
@@ -0,0 +1,15 @@
+package cn.edu.zucc.graduationproject.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/login")
+    public String login(ModelMap map){
+        String hello="helloword i'm cjc-cjc";
+        map.put("hello",hello);
+        return "login";
+    }
+}
